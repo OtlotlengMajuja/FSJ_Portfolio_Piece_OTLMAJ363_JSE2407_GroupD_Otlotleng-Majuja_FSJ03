c7525227f8f5084d6d397b4d4cfdbb161d9ed631
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-10-07 20:24:10 +0200

    Updated categories route to return full doc data, and changed product API URL to relative path

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -5,7 +5,7 @@ import { collection, getDocs } from 'firebase/firestore';
 export async function GET() {
     try {
         const categoriesSnapshot = await getDocs(collection(db, 'categories', "allCategories"));
-        const categories = categoriesSnapshot.docs.map(doc => doc.data().name);
+        const categories = categoriesSnapshot.docs.map(doc => doc.data());
 
         return new Response(JSON.stringify(categories), {
             headers: { 'Content-Type': 'application/json' },
